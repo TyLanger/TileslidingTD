c2fb13dcb119baf3f8af6db7d942088d5d6722f8
TyLanger <ty.langer16@gmail.com>
2023-07-08 13:58:37 -0600

    Double Wrap Bug Fix
    If you wrap twice, the game doesn't crash
    Also made the tiles settling in to their new position more efficient. It
    was running every frame before and now it only runs when you release the
    mouse.

--- tile_grid/tile_grid.gd
+++ tile_grid/tile_grid.gd
@@ -30,6 +30,8 @@ func _ready():
 			#tile.modulate = Color(i/10.0, j/10.0, 1.0-(i+j)/20.0)
 			if(i==0 || j==0 || i==13 || j==6 || i==6 || i==7):
 				tile.modulate = Color(1.0, 0.6, 0.6)
+			else:
+				tile.modulate = Color(0.8 + j/10.0, 0.8+j/10.0, 0.8+j/10.0)
 			add_child(tile)
 			row.append(tile)
 			#grid[i][j] = tile
@@ -57,11 +59,10 @@ func _process(delta):
 		var mouse = get_viewport().get_mouse_position()
 		print("unclick at: ", mouse)
 		dragging = false
+		reset_col(col_drag)
 		
 	if(dragging):
 		change_col(col_drag)
-	else:
-		reset_col(col_drag)
 	
 func _input(event):
    # Mouse in viewport coordinates.
@@ -96,22 +97,48 @@ func change_col(col_index):
 		# if height < min, height = max
 		
 func reset_col(col_index):
+	# bug scenarios
+	# drag down out of bounds. The last tile index is >5
+	# drag up out of bounds. Multiple indicies are 0
+	# loop over all indicies. If any are 0, error. If any are 6, error
+	# if any are 0, they should be 1,2,3,4,5
+	# if any are 6, they should be 1,2,3,4,5
+	
+	# temp col to copy/store tiles
+	# so they aren't swapped while being iterated over
+	var temp_col = []
+	temp_col.resize(5)
+	var double_wrap = false
+	
 	for i in range(1,6):
-		#grid[col_index][i].modulate = Color(0.9 + i/10.0, 0.9 + i/10.0, 0.9 + i/10.0)
-		#grid[col_index][i].position = Vector2(offset + tile_width * col_index, offset + tile_width * i)
-		# have they moved?
-		# want to save this new pos when they are dropped
 		var pos = grid[col_index][i].position
 		var index: int = (pos.y - (offset - (tile_width/2))) / tile_width
-		var anchor_y_pos = offset + tile_width * index
-		grid[col_index][i].position = Vector2(pos.x, anchor_y_pos)
-		#swap in grid
-		var temp = grid[col_index][i]
-		grid[col_index][i] = grid[col_index][index]
-		grid[col_index][index] = temp
-		# if you drag the top tile around twice, it stops looping and will crash
-		# if you drag from the bottom around twice, multiple go to 0 I think
-		# maybe as soon as one gets out of bounds, I should shift all of them		
+		if(index==0 || index>5):
+			double_wrap = true
+			break
+	
+	if(double_wrap):
+		for i in range(1,6):
+			var pos = grid[col_index][i].position
+			var anchor_y_pos = offset + tile_width * i
+			grid[col_index][i].position = Vector2(pos.x, anchor_y_pos)
+	else:
+		for i in range(1,6):
+			#grid[col_index][i].modulate = Color(0.9 + i/10.0, 0.9 + i/10.0, 0.9 + i/10.0)
+			#grid[col_index][i].position = Vector2(offset + tile_width * col_index, offset + tile_width * i)
+			# have they moved?
+			# want to save this new pos when they are dropped
+			var pos = grid[col_index][i].position
+			var index: int = (pos.y - (offset - (tile_width/2))) / tile_width
+			#print("index: ", index)
+			var anchor_y_pos = offset + tile_width * index
+			grid[col_index][i].position = Vector2(pos.x, anchor_y_pos)
+			temp_col[index-1] = grid[col_index][i]
+		# if you double wrap, the tiles are already in the right order
+		for i in 5:
+			grid[col_index][i+1] = temp_col[i]
+
+
 
 func swap_tiles_above(col_index):
 	#dragging up, the top tile is out of bounds, wrap around to the bottom
